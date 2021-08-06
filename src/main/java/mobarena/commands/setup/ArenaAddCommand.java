37eea2f8562ca06c13f29c124fe11e45fb2396ad
Ommp <dallande@protonmail.com>
2021-08-07 01:01:18 +0200

    Changed the way the command is registered, also added arguments for arena name and all 6 coordinates needed for the main fighting area of the arena.

--- src/main/java/mobarena/commands/setup/ArenaAddCommand.java
+++ src/main/java/mobarena/commands/setup/ArenaAddCommand.java
@@ -1,31 +1,50 @@
-package mobarena.commands;
+package mobarena.commands.setup;
 
-import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.Command;
+import mobarena.ArenaLocation;
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 
-public class ArenaAddCommand implements Command<ServerCommandSource> {
+public class ArenaAddCommand {
 
-    public ArenaAddCommand() {}
-    
-    @Override
-    public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        ServerCommandSource source = context.getSource();
-        ServerPlayerEntity senderPlayer = source.getPlayer();
+public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+    dispatcher.register(CommandManager.literal("createarena")
+                    .then(CommandManager.argument("arenaname", StringArgumentType.word())
+                        .then(CommandManager.argument("x_1", DoubleArgumentType.doubleArg())
+                        .then(CommandManager.argument("y_1", DoubleArgumentType.doubleArg())
+                        .then(CommandManager.argument("z_1", DoubleArgumentType.doubleArg())
+                        .then(CommandManager.argument("x_2", DoubleArgumentType.doubleArg())
+                        .then(CommandManager.argument("y_2", DoubleArgumentType.doubleArg())
+                        .then(CommandManager.argument("z_2", DoubleArgumentType.doubleArg())
 
-        //store Arena name
-        String arenaName = StringArgumentType.getString(context, "arena_name");
-        System.out.println("this is just a test to see if the command works!");
-        // try {
-            
-        // } catch {
+                        .executes(context -> {
+                                    final ServerCommandSource source = context.getSource();
+                                    ServerPlayerEntity senderPlayer = source.getPlayer();
 
-        // }
-            return 1;
+                                        String arenaName = StringArgumentType.getString(context, "arenaname");
+                                        double x_1 = DoubleArgumentType.getDouble(context, "x_1");
+                                        double y_1 = DoubleArgumentType.getDouble(context, "y_1");
+                                        double z_1 = DoubleArgumentType.getDouble(context, "z_1");
+                                        double x_2 = DoubleArgumentType.getDouble(context, "x_2");
+                                        double y_2 = DoubleArgumentType.getDouble(context, "y_2");
+                                        double z_2 = DoubleArgumentType.getDouble(context, "z_2");
+//                                      ArenaLocation pos1 = new ArenaLocation(senderPlayer.world.getRegistryKey(), x_1, y_1, z_1);
+                                        ArenaLocation pos2 = new ArenaLocation(senderPlayer.world.getRegistryKey(), 80, 85, 100);
+
+                                    System.out.println("Created an arena with name " + arenaName + " at: ");
+//                                    System.out.println(pos1.pos.y);
+//                                    System.out.println(pos1.dim.getValue());
+
+
+                            return 1;
+                    })))))))));
+        }
+
     }
-}
-
