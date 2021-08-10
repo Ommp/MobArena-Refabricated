1ec84a7ad16b48b1b68daf2831c3194c40785ff1
Ommp <dallande@protonmail.com>
2021-08-10 02:29:24 +0200

    Feedback is now sent to the player instead of the console when creating an arena.

--- src/main/java/mobarena/commands/setup/ArenaAddCommand.java
+++ src/main/java/mobarena/commands/setup/ArenaAddCommand.java
@@ -16,35 +16,25 @@ public class ArenaAddCommand {
 
 public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
     dispatcher.register(CommandManager.literal("createarena")
-                    .then(CommandManager.argument("arenaname", StringArgumentType.word())
-                        .then(CommandManager.argument("x_1", DoubleArgumentType.doubleArg())
-                        .then(CommandManager.argument("y_1", DoubleArgumentType.doubleArg())
-                        .then(CommandManager.argument("z_1", DoubleArgumentType.doubleArg())
-                        .then(CommandManager.argument("x_2", DoubleArgumentType.doubleArg())
-                        .then(CommandManager.argument("y_2", DoubleArgumentType.doubleArg())
-                        .then(CommandManager.argument("z_2", DoubleArgumentType.doubleArg())
+            .then(CommandManager.argument("arenaname", StringArgumentType.word())
+            .then(CommandManager.argument("x_1", DoubleArgumentType.doubleArg())
+            .then(CommandManager.argument("y_1", DoubleArgumentType.doubleArg())
+            .then(CommandManager.argument("z_1", DoubleArgumentType.doubleArg())
+            .then(CommandManager.argument("x_2", DoubleArgumentType.doubleArg())
+            .then(CommandManager.argument("y_2", DoubleArgumentType.doubleArg())
+            .then(CommandManager.argument("z_2", DoubleArgumentType.doubleArg())
 
                         .executes(context -> {
-                                    final ServerCommandSource source = context.getSource();
-                                    ServerPlayerEntity senderPlayer = source.getPlayer();
+                            final ServerCommandSource source = context.getSource();
+                            ServerPlayerEntity senderPlayer = source.getPlayer();
 
-                                        String arenaName = StringArgumentType.getString(context, "arenaname");
-                                        double x_1 = DoubleArgumentType.getDouble(context, "x_1");
-                                        double y_1 = DoubleArgumentType.getDouble(context, "y_1");
-                                        double z_1 = DoubleArgumentType.getDouble(context, "z_1");
-                                        double x_2 = DoubleArgumentType.getDouble(context, "x_2");
-                                        double y_2 = DoubleArgumentType.getDouble(context, "y_2");
-                                        double z_2 = DoubleArgumentType.getDouble(context, "z_2");
-//                                      ArenaLocation pos1 = new ArenaLocation(senderPlayer.world.getRegistryKey(), x_1, y_1, z_1);
-                                        ArenaLocation pos2 = new ArenaLocation(senderPlayer.world.getRegistryKey(), 80, 85, 100);
+                            String arenaName = StringArgumentType.getString(context, "arenaname");
+                            ArenaLocation pos1 = new ArenaLocation(senderPlayer.world.getRegistryKey(), DoubleArgumentType.getDouble(context, "x_1"), DoubleArgumentType.getDouble(context, "y_1"), DoubleArgumentType.getDouble(context, "z_1"));
+                            ArenaLocation pos2 = new ArenaLocation(senderPlayer.world.getRegistryKey(), DoubleArgumentType.getDouble(context, "x_2"), DoubleArgumentType.getDouble(context, "y_2"), DoubleArgumentType.getDouble(context, "z_2"));
 
-                                    System.out.println("Created an arena with name " + arenaName + " at: ");
-//                                    System.out.println(pos1.pos.y);
-//                                    System.out.println(pos1.dim.getValue());
-
-
+                            context.getSource().sendFeedback(new LiteralText("Created arena: " + arenaName), true);
+                            context.getSource().sendFeedback(new LiteralText("From: " + pos1.pos + " to: " + pos2.pos), true);
                             return 1;
                     })))))))));
         }
-
     }
