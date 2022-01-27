bfc49ea83f2aa9600203f1bb6863a3eb8de35843
Ommp <dallande@protonmail.com>
2022-01-27 11:09:01 +0100

    temporary commit before solving type issue

--- src/main/java/mobarena/config/MobArenaConfig.java
+++ src/main/java/mobarena/config/MobArenaConfig.java
@@ -1,43 +1,79 @@
 package mobarena.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MobArenaConfig {
 
     private static MobArenaConfig INSTANCE=null;
 
-
     public boolean globalEnabled = true;
 
-    public JsonObject arenasJsonObject;
 
-    public class ArenaContainer {
-        public List<ArenaTemplate> arenasList;
-    }
 
-    public class ArenaTemplate {
-        public String name;
+//        public String name;
+//
+//        public int dimensionId;
+//        public boolean arenaEnabled = true;
+//
+//        public List<Double> arenaWarp;
+//        public List<Double> lobbyWarp;
+//        public List<Double> exitWarp;
+//        public List<Double> specWarp;
+//
+//        public List<Float> arenaWarpYawPitch, lobbyWarpYawPitch, exitWarpPitch, specWarpYawPitch;
+//
+//        public List<Integer> p1, p2, l1, l2;
 
-        public int dimensionId;
-        public boolean arenaEnabled = true;
 
-        public List<Double> arenaWarp;
-        public List<Double> lobbyWarp;
-        public List<Double> exitWarp;
-        public List<Double> specWarp;
+    public JsonObject ArenaDataTemplate(String name, int dimensionId, boolean arenaEnabled,
+                                        List<Double> arenaWarp, List<Double> lobbyWarp, List<Double> exitWarp, List<Double> specWarp,
+                                        List<Float> arenaWarpYawPitch,  List<Float> lobbyWarpYawPitch,  List<Float> exitWarpPitch,  List<Float> specWarpYawPitch,
+                                        ArrayList<Integer> p1, List<Integer> p2, List<Integer> l1, List<Integer> l2) {
 
-        public List<Float> arenaWarpYawPitch, lobbyWarpYawPitch, exitWarpPitch, specWarpYawPitch;
+        JsonArray pointOne = new JsonArray();
+        pointOne.addAll(arenaWarp);
 
-        public List<Integer> p1, p2, l1, l2;
+//        JsonArray arenaWarpTest = new JsonArray();
+//        arenaWarpTest.add(50);
+//        arenaWarpTest.add(50.5);
+//        arenaWarpTest.add(70.5);
+//
+//        arena.add("arenaWarp", arenaWarpTest);
+
+
+
+
+        JsonObject arena = new JsonObject();
+        arena.add("dimensionId", new JsonPrimitive(dimensionId));
+        arena.add("arenaEnabled", new JsonPrimitive(true));
+
+        arena.add("arenaWarp", new JsonPrimitive((Number) arenaWarp));
+        arena.add("lobbyWarp", new JsonPrimitive((Number) lobbyWarp));
+        arena.add("exitWarp", new JsonPrimitive((Number) exitWarp));
+        arena.add("specWarp", new JsonPrimitive((Number) specWarp));
+
+        arena.add("arenaWarpYawPitch", new JsonPrimitive((Number) arenaWarpYawPitch));
+        arena.add("lobbyWarpYawPitch", new JsonPrimitive((Number) lobbyWarpYawPitch));
+        arena.add("exitWarpPitch", new JsonPrimitive((Number) exitWarpPitch));
+        arena.add("specWarpYawPitch", new JsonPrimitive((Number) specWarpYawPitch));
+
+        arena.add("p1", new JsonArray(p1));
+        arena.add("p2", new JsonPrimitive((Number) p2));
+        arena.add("l1", new JsonPrimitive((Number) l1));
+        arena.add("l2", new JsonPrimitive((Number) l2));
+
+        JsonObject list = new JsonObject();
+        list.add(name, arena);
+
+        return list;
     }
 
 
