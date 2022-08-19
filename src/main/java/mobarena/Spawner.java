314c2f3de9bc876ef470bb8e7ba486e0db83b216
Ommp <dallande@protonmail.com>
2022-08-19 15:33:36 +0200

    refactor prepareSpawner method to not need an if statement for every possible mob that could spawn.

--- src/main/java/mobarena/Spawner.java
+++ src/main/java/mobarena/Spawner.java
@@ -1,16 +1,19 @@
 package mobarena;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.mob.*;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3i;
 
 import java.util.ArrayList;
 
 public class Spawner {
-    private ArrayList<EntityType> potentialMonsters = new ArrayList<>();
+    private ArrayList<String> potentialMobs = new ArrayList<>();
     private ArrayList<HostileEntity> monsters = new ArrayList<>();
 
     private int deadMonsters;
@@ -18,26 +21,27 @@ public class Spawner {
     private ServerWorld world;
 
     public void addPotentialMonsters() {
-        potentialMonsters.add(EntityType.ZOMBIE);
-        potentialMonsters.add(EntityType.HUSK);
-        potentialMonsters.add(EntityType.BLAZE);
-        potentialMonsters.add(EntityType.SPIDER);
+        potentialMobs.add("minecraft:zombie");
+        potentialMobs.add("minecraft:husk");
+        potentialMobs.add("minecraft:blaze");
+        potentialMobs.add("minecraft:spider");
+        potentialMobs.add("minecraft:skeleton");
+        potentialMobs.add("minecraft:pillager");
     }
     public void prepareSpawner(int mobsToSpawn, WaveType waveType) {
         for (int i = 0; i < mobsToSpawn; i++) {
-            int index = (int)(Math.random() * potentialMonsters.size());
-            if (potentialMonsters.get(index) == EntityType.ZOMBIE) {
-                monsters.add(new ZombieEntity(EntityType.ZOMBIE, world));
+            int index = (int)(Math.random() * potentialMobs.size());
+
+
+//TODO fix skeletons and pillagers spawning without equipment
+            NbtCompound nbtCompound = new NbtCompound();
+            nbtCompound.putString("id", potentialMobs.get(index));
+//            Optional<EntityType<?>> optional = EntityType.fromNbt(nbtCompound);
+            Entity entity = EntityType.loadEntityWithPassengers(nbtCompound, world, entity2 -> entity2);
+            if (entity instanceof HostileEntity) {
+                monsters.add((HostileEntity) entity);
             }
-            else if (potentialMonsters.get(index) == EntityType.HUSK) {
-                monsters.add(new HuskEntity(EntityType.HUSK, world));
-            }
-            else if (potentialMonsters.get(index) == EntityType.SPIDER) {
-                monsters.add(new SpiderEntity(EntityType.SPIDER, world));
-            }
-            else if (potentialMonsters.get(index) == EntityType.BLAZE) {
-                monsters.add(new BlazeEntity(EntityType.BLAZE, world));
-            }
+
             if (waveType.equals(WaveType.BOSS)) {
                 monsters.get(i).addStatusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 1000000, 3));
                 monsters.get(i).addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 1000000, 2));
@@ -52,7 +56,7 @@ public class Spawner {
     }
 
     public void spawnMobs(){
-        for (HostileEntity entity: monsters) {
+        for (MobEntity entity: monsters) {
             Vec3i spawnPoint = MobArena.arenaManager.arenas.get(arenaName).getRandomSpawnPoint();
             MobArena.arenaManager.connectMobToArena(entity.getUuidAsString(), arenaName);
             entity.updatePosition(spawnPoint.getX(), spawnPoint.getY(), spawnPoint.getZ());
