cac6465fd56700c2950a1ea95a246223d7adb22f
Ommp <dallande@protonmail.com>
2022-09-15 15:07:37 +0200

    Cancel mobs burning in daylight if they belong to the arena

---
+++ src/main/java/mobarena/mixin/MobEntityMixin.java
@@ -0,0 +1,20 @@
+package mobarena.mixin;
+
+import mobarena.ArenaManager;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin {
+    @Inject(method = "isAffectedByDaylight", at = @At("RETURN"), cancellable = true)
+    private void disableAffectedByDaylight(CallbackInfoReturnable<Boolean> cir) {
+        LivingEntity entity = (LivingEntity) (Object) this;
+        if (ArenaManager.getMobToArena().containsKey(entity.getUuidAsString())) {
+            cir.setReturnValue(false);
+        }
+    }
+}
