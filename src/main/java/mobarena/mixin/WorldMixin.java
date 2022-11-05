923a3164d25ea85a4e8383a93f1b8c17eb653359
Ommp <dallande@protonmail.com>
2022-11-05 11:22:09 +0100

    Remove injection that cancels destruction of blocks because it causes unwanted behaviour. Inject into explosions so that they harm the entities but don't destroy blocks. Mixin into ServerWorld instead of World.

--- src/main/java/mobarena/mixin/WorldMixin.java
+++ src/main/java/mobarena/mixin/WorldMixin.java
@@ -1,23 +1,27 @@
 package mobarena.mixin;
 
 import mobarena.ArenaManager;
-import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.explosion.Explosion;
+import net.minecraft.world.explosion.ExplosionBehavior;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(World.class)
+@Mixin(ServerWorld.class)
 public abstract class WorldMixin {
 
-
-    @Inject(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;II)Z", at = @At("HEAD"), cancellable = true)
-    private void inject(BlockPos pos, BlockState state, int flags, int maxUpdateDepth, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "createExplosion", at = @At("HEAD"), cancellable = true)
+    private void inject(Entity entity, DamageSource damageSource, ExplosionBehavior behavior, double x, double y, double z, float power, boolean createFire, Explosion.DestructionType destructionType, CallbackInfoReturnable<Explosion> cir) {
         for (var arena: ArenaManager.arenas.values()) {
-            if (arena.getArenaRegion().isInsideRegion(pos) && arena.getIsProtected()) {
-                cir.setReturnValue(false);
+            if (arena.getArenaRegion().isInsideRegion(entity.getBlockPos()) && arena.getIsProtected()) {
+                Explosion noharmExplosion = new Explosion(entity.getEntityWorld(), entity, x, y, z, power, createFire, Explosion.DestructionType.NONE);
+                noharmExplosion.collectBlocksAndDamageEntities();
+                cir.setReturnValue(noharmExplosion);
+
                 break;
             }
         }
