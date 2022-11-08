c7b0ee85cf23efe94e8f8dbead3e8e0699d900a1
Ommp <dallande@protonmail.com>
2022-11-08 13:02:13 +0100

    Fix explosion particles not working

--- src/main/java/mobarena/mixin/WorldMixin.java
+++ src/main/java/mobarena/mixin/WorldMixin.java
@@ -3,17 +3,25 @@ package mobarena.mixin;
 import mobarena.ArenaManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.explosion.Explosion;
 import net.minecraft.world.explosion.ExplosionBehavior;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(ServerWorld.class)
 public abstract class WorldMixin {
+
+    @Shadow public abstract List<ServerPlayerEntity> getPlayers();
 
     @Inject(method = "createExplosion", at = @At("HEAD"), cancellable = true)
     private void inject(Entity entity, DamageSource damageSource, ExplosionBehavior behavior, double x, double y, double z, float power, boolean createFire, Explosion.DestructionType destructionType, CallbackInfoReturnable<Explosion> cir) {
@@ -23,7 +31,12 @@ public abstract class WorldMixin {
 
                     Explosion noharmExplosion = new Explosion(arena.getWorld(), entity, x, y, z, power, createFire, Explosion.DestructionType.NONE);
                     noharmExplosion.collectBlocksAndDamageEntities();
-                    noharmExplosion.affectWorld(true);
+                    noharmExplosion.affectWorld(false);
+
+                for (ServerPlayerEntity serverPlayerEntity : this.getPlayers()) {
+                    if (!(serverPlayerEntity.squaredDistanceTo(x, y, z) < 4096.0)) continue;
+                    serverPlayerEntity.networkHandler.sendPacket(new ExplosionS2CPacket(x, y, z, power, new ArrayList<>(), noharmExplosion.getAffectedPlayers().get(serverPlayerEntity)));
+                }
                     cir.setReturnValue(noharmExplosion);
 
                     break;
