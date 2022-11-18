cd0e8ba50c327487e0687be83f2ddf78779b5d2c
Ommp <dallande@protonmail.com>
2022-11-18 09:49:54 +0100

    Stop endermen from picking and placing blocks

---
+++ src/main/java/mobarena/mixin/EndermanPlaceMixin.java
@@ -0,0 +1,25 @@
+package mobarena.mixin;
+
+import mobarena.ArenaManager;
+import net.minecraft.entity.mob.EndermanEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net/minecraft/entity/mob/EndermanEntity$PlaceBlockGoal")
+public class EndermanPlaceMixin {
+
+    @Shadow @Final private EndermanEntity enderman;
+
+    @Inject(method = "canStart", at = @At("HEAD"), cancellable = true)
+    private void inject(CallbackInfoReturnable<Boolean> cir) {
+        for (var arena : ArenaManager.arenas.values()) {
+            if (arena.getArenaRegion().isInsideRegion(enderman.getBlockPos()) && arena.getIsProtected()) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
