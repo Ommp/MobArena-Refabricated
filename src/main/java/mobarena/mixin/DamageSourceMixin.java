b563a1c2a1d4db3bc83587aee497d7dd4fa68198
Ommp <dallande@protonmail.com>
2023-01-12 10:53:42 +0100

    Add player damage and kill count for ranged actions. Tested to work with vanilla bow/crossbow and with guns and grenades from HWG.

--- src/main/java/mobarena/mixin/DamageSourceMixin.java
+++ src/main/java/mobarena/mixin/DamageSourceMixin.java
@@ -4,6 +4,7 @@ import mobarena.ArenaManager;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageTracker;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,7 +21,13 @@ public abstract class DamageSourceMixin {
     private void inject(DamageSource damageSource, float originalHealth, float damage, CallbackInfo ci) {
         if (damageSource.getSource() instanceof ServerPlayerEntity p) {
             if (ArenaManager.isPlayerActive(p)) {
-                ArenaManager.getArenaFromPlayer(p).getScoreboard().increasePlayerDamage(p, originalHealth-getEntity().getHealth());
+                ArenaManager.getArenaFromPlayer(p).getScoreboard().increasePlayerDamage(p, originalHealth - getEntity().getHealth());
+            }
+        } else if (damageSource.getSource() instanceof PersistentProjectileEntity projectile) {
+            if (projectile.getOwner() instanceof ServerPlayerEntity player) {
+                if (ArenaManager.isPlayerActive(player)) {
+                    ArenaManager.getArenaFromPlayer(player).getScoreboard().increasePlayerDamage(player, originalHealth - getEntity().getHealth());
+                }
             }
         }
     }
